e6ff840a90a8083d2c28feea96ead87eecd23731
ravillam1 <91558075+ravillam1@users.noreply.github.com>
2026-03-09 20:13:27 +0100

    Algun comentario más + organización

--- src/main/java/proyectoPunto4/DB.java
+++ src/main/java/proyectoPunto4/DB.java
@@ -5,11 +5,39 @@ import java.util.List;
  * Interfaz que representa a la base de datos
  */
 public interface DB {
+	/*
+	 * Almacenar un ToDo
+	 * @param t ToDo a almacenar
+	 */
 	public void createTodo(ToDo t);
+	/*
+	 * Obtener un ToDo
+	 * @param id id del ToDo
+	 */
 	public ToDo readTodo(String id);
+	/*
+	 * Actualizar ToDo
+	 * @param t ToDo a actualizar
+	 */
 	public void updateTodo(ToDo t);
+	/*
+	 * Borrar ToDo
+	 * @param t ToDo a borrar
+	 */
 	public void deleteTodo(ToDo t);
+	/*
+	 * Guardar email
+	 * @param email email a guardar
+	 */
 	public void saveEmail(String email);
+	/*
+	 * Obtener todos los emails
+	 * @return lista de emails de la BD
+	 */
 	public List<String> getEmails();
+	/*
+	 * Obtener todos los ToDos
+	 * @return lista de ToDos de la BD
+	 */
 	public List<ToDo> getAllTodos();
 }
--- src/main/java/proyectoPunto4/Mailer.java
+++ src/main/java/proyectoPunto4/Mailer.java
@@ -3,5 +3,10 @@ package proyectoPunto4;
  * Interfaz que representa al Mailer
  */
 public interface Mailer {
+	/*
+	 * Envía un correo
+	 * @param sireccion direccion a donde se envía
+	 * @param mensaje mensaje del correo
+	 */
 	public boolean enviarCorreo(String sireccion, String mensaje);
 }
